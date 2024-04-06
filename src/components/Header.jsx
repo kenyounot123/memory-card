bdc77cb8c5ef0e20b0ddcd6f9ef128e2464d66ad
kenyounot123 <kenlu519@gmail.com>
2024-04-06 16:28:55 -0400

    Create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import "../stylesheets/Header.css";
+function Header() {
+  return (
+    <h1 className="header-title">
+      Do You Got That <span className="highlight">DOG</span> In You ?
+    </h1>
+  );
+}
+export { Header };
