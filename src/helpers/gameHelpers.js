cc7843190729e92ab1bc619bec46bf122039c8f9
Ken Lu <kenlu519@gmail.com>
2024-04-07 12:31:08 -0400

    Update shuffling algorithm

--- src/helpers/gameHelpers.js
+++ src/helpers/gameHelpers.js
@@ -1,11 +1,16 @@
-function shuffleCards(dogData, setDogData, numberOfDogs) {
-  const newDogArray = new Array(numberOfDogs).fill(null);
-  // Creates a new array of dogData in randomized order
-  for (let i = 0; i < newDogArray.length; i++) {
-    const randomIndex = Math.floor(Math.random() * (numberOfDogs + 1));
-    newDogArray[randomIndex] = dogData[i];
-  }
-  setDogData(newDogArray);
-}
+function shuffleCards(dogData, setDogData) {
+  const shuffledArray = [...dogData];
 
+  // Shuffle the copied array using Fisher-Yates algorithm
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * (i + 1)); // Generate random index
+    // Swap elements at current index and random index
+    [shuffledArray[i], shuffledArray[randomIndex]] = [
+      shuffledArray[randomIndex],
+      shuffledArray[i],
+    ];
+  }
+
+  setDogData(shuffledArray);
+}
 export { shuffleCards };
